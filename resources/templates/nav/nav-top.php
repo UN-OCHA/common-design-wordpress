2e9d2833397e68ca03c63a141ebbd839440f21c0
left23 <lcdemc@gmail.com>
2021-06-02 12:01:48 +0200

    start to integrate the main menu and place it in the correct location

--- resources/templates/nav/nav-top.php
+++ resources/templates/nav/nav-top.php
@@ -6,7 +6,8 @@
  */
 ?>
 
-<nav>
+<nav role="navigation" id="block-mainnavigation" aria-labelledby="block-mainnavigation-menu" data-cd-toggable="Menu" data-cd-component="cd-nav" data-cd-icon="arrow-down" class="block block-menu navigation menu--main cd-nav cd-site-header__nav">
+    <h2 id="block-mainnavigation-menu">Main navigation</h2>
 	<?php
 		wp_nav_menu( array(
 			'theme_location' => 'common_design_top',
--- resources/templates/parts/parts-header.php
+++ resources/templates/parts/parts-header.php
@@ -6,7 +6,7 @@
  */
 ?>
 
-	
+
 
 <header class="cd-header">
 
@@ -35,15 +35,15 @@
 	    		<!-- blue ocha logo-->
 	  			<div class="region region-header-logo">
 	  			  	<!-- <a href="/" title="Home" rel="home" class="cd-site-logo"> -->
-	  			  	 <?php the_Custom_logo(); ?> 
-	  					
+	  			  	 <?php the_Custom_logo(); ?>
+
 
 	  						<!-- question : do I need to wrap the custom logo php in img? Also I don't know why if I don't use </img> it shows the last php angle bracket -->
 					<!-- </a> -->
 	  			</div>
 
 	  	<!-- menu connected for wp found in nav-top.php -->
-	
+
 
 
 	  			<!-- menu options container-->
@@ -79,57 +79,56 @@
 	  				</div>
 
 					<div class="cd-site-header__nav-holder">
-						<nav role="navigation" id="block-mainnavigation" aria-labelledby="block-mainnavigation-menu" data-cd-toggable="Menu" data-cd-component="cd-nav" data-cd-icon="arrow-down" class="block block-menu navigation menu--main cd-nav cd-site-header__nav" data-cd-processed="true">
-	  						<h2 id="block-mainnavigation-menu">Main navigation</h2>
-							<ul class="menu">
-						    	<li class="menu-item">
-									<a href="/demo" id="cd-main-menu-item-0-1"><span>Common design demo</span></a>
-									<!-- common design menu button -->
-						    	</li>
-	     						<li class="menu-item">
-									<a href="/svg-icon-library" id="cd-main-menu-item-0-2"><span>Icons</span></a>
-								</li>
-								<li class="menu-item">
-									<a href="https://brand.unocha.org" id="cd-main-menu-item-0-3"><span>Brand guidelines</span></a>
-								</li>
-	 							<li class="menu-item menu-item--expanded">
-	    							<button type="button" id="cd-main-menu-0-4-toggler" class="cd-main-menu__btn" data-cd-toggler="" aria-expanded="false" aria-haspopup="true" aria-controls="cd-main-menu-0-4"><span class="cd-main-menu__btn-label">Layouts</span><svg width="16" height="16" aria-hidden="true" focusable="false" class="cd-icon cd-icon--arrow-down cd-main-menu__icon"><use xlink:href="#cd-icon--arrow-down"></use></svg></button>
-	    							<ul data-cd-toggable="Layouts" data-cd-icon="arrow-down" data-cd-component="cd-main-menu" id="cd-main-menu-0-4" class="menu cd-main-menu__dropdown" data-cd-hidden="true" data-cd-processed="true">
-								        <li class="menu-item">
-											<a href="/node/1" id="cd-main-menu-item-1-1"><span>First sidebar</span></a>
-								    	</li>
-								      	<li class="menu-item">
-								            <a href="/node/2" id="cd-main-menu-item-1-2"><span>Second sidebar</span></a>
-								    	</li>
-									    <li class="menu-item">
-											<a href="/node/3" id="cd-main-menu-item-1-3"><span>Both sidebars</span></a>
-									    </li>
-									</ul>
-								</li>
-	      						<li class="menu-item menu-item--expanded">
-	      							<button type="button" id="cd-main-menu-0-5-toggler" class="cd-main-menu__btn" data-cd-toggler="" aria-expanded="false" aria-haspopup="true" aria-controls="cd-main-menu-0-5"><span class="cd-main-menu__btn-label">Views List</span><svg width="16" height="16" aria-hidden="true" focusable="false" class="cd-icon cd-icon--arrow-down cd-main-menu__icon"><use xlink:href="#cd-icon--arrow-down"></use></svg></button>
-	      							<ul data-cd-toggable="Views List" data-cd-icon="arrow-down" data-cd-component="cd-main-menu" id="cd-main-menu-0-5" class="menu cd-main-menu__dropdown" data-cd-hidden="true" data-cd-processed="true">
-	      								<li class="menu-item">
-	      									<a href="/teaser-list" id="cd-main-menu-item-1-1"><span>Teaser List</span></a>
-	      								</li>
-									    <li class="menu-item">
-											<a href="/card-list" id="cd-main-menu-item-1-2"><span>Card List</span></a>
-									    </li>
-									    <li class="menu-item">
-											<a href="/facets" id="cd-main-menu-item-1-3"><span>List with facets</span></a>
-									    </li>
+						<?php get_template_part( 'resources/templates/nav/nav', 'top' ); ?>
 
-	       							</ul>
-	       						</li>
-	       					 </ul>
-						</nav>
+
+<!--						<nav role="navigation" id="block-mainnavigation" aria-labelledby="block-mainnavigation-menu" data-cd-toggable="Menu" data-cd-component="cd-nav" data-cd-icon="arrow-down" class="block block-menu navigation menu--main cd-nav cd-site-header__nav" data-cd-processed="true">-->
+<!--	  						<h2 id="block-mainnavigation-menu">Main navigation</h2>-->
+<!--							<ul class="menu">-->
+<!--						    	<li class="menu-item">-->
+<!--									<a href="/demo" id="cd-main-menu-item-0-1"><span>Common design demo</span></a>-->
+<!--						    	</li>-->
+<!--	     						<li class="menu-item">-->
+<!--									<a href="/svg-icon-library" id="cd-main-menu-item-0-2"><span>Icons</span></a>-->
+<!--								</li>-->
+<!--								<li class="menu-item">-->
+<!--									<a href="https://brand.unocha.org" id="cd-main-menu-item-0-3"><span>Brand guidelines</span></a>-->
+<!--								</li>-->
+<!--	 							<li class="menu-item menu-item--expanded">-->
+<!--	    							<button type="button" id="cd-main-menu-0-4-toggler" class="cd-main-menu__btn" data-cd-toggler="" aria-expanded="false" aria-haspopup="true" aria-controls="cd-main-menu-0-4"><span class="cd-main-menu__btn-label">Layouts</span><svg width="16" height="16" aria-hidden="true" focusable="false" class="cd-icon cd-icon--arrow-down cd-main-menu__icon"><use xlink:href="#cd-icon--arrow-down"></use></svg></button>-->
+<!--	    							<ul data-cd-toggable="Layouts" data-cd-icon="arrow-down" data-cd-component="cd-main-menu" id="cd-main-menu-0-4" class="menu cd-main-menu__dropdown" data-cd-hidden="true" data-cd-processed="true">-->
+<!--								        <li class="menu-item">-->
+<!--											<a href="/node/1" id="cd-main-menu-item-1-1"><span>First sidebar</span></a>-->
+<!--								    	</li>-->
+<!--								      	<li class="menu-item">-->
+<!--								            <a href="/node/2" id="cd-main-menu-item-1-2"><span>Second sidebar</span></a>-->
+<!--								    	</li>-->
+<!--									    <li class="menu-item">-->
+<!--											<a href="/node/3" id="cd-main-menu-item-1-3"><span>Both sidebars</span></a>-->
+<!--									    </li>-->
+<!--									</ul>-->
+<!--								</li>-->
+<!--	      						<li class="menu-item menu-item--expanded">-->
+<!--	      							<button type="button" id="cd-main-menu-0-5-toggler" class="cd-main-menu__btn" data-cd-toggler="" aria-expanded="false" aria-haspopup="true" aria-controls="cd-main-menu-0-5"><span class="cd-main-menu__btn-label">Views List</span><svg width="16" height="16" aria-hidden="true" focusable="false" class="cd-icon cd-icon--arrow-down cd-main-menu__icon"><use xlink:href="#cd-icon--arrow-down"></use></svg></button>-->
+<!--	      							<ul data-cd-toggable="Views List" data-cd-icon="arrow-down" data-cd-component="cd-main-menu" id="cd-main-menu-0-5" class="menu cd-main-menu__dropdown" data-cd-hidden="true" data-cd-processed="true">-->
+<!--	      								<li class="menu-item">-->
+<!--	      									<a href="/teaser-list" id="cd-main-menu-item-1-1"><span>Teaser List</span></a>-->
+<!--	      								</li>-->
+<!--									    <li class="menu-item">-->
+<!--											<a href="/card-list" id="cd-main-menu-item-1-2"><span>Card List</span></a>-->
+<!--									    </li>-->
+<!--									    <li class="menu-item">-->
+<!--											<a href="/facets" id="cd-main-menu-item-1-3"><span>List with facets</span></a>-->
+<!--									    </li>-->
+<!---->
+<!--	       							</ul>-->
+<!--	       						</li>-->
+<!--	       					 </ul>-->
+<!--						</nav>-->
 					</div>
 
     		</div>
-  		</div> 
-
-  		<?php get_template_part( 'resources/templates/nav/nav', 'top' ); ?>
-
+  		</div>
 
 </header>
 
