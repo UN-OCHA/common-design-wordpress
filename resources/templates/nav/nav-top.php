5b370e8c4019ed8e98c814a77c8e3b7a4784f4b2
left23 <lcdemc@gmail.com>
2021-06-03 16:26:48 +0200

    remove extra markup used for testing

--- resources/templates/nav/nav-top.php
+++ resources/templates/nav/nav-top.php
@@ -6,7 +6,14 @@
  */
 ?>
 
-<nav role="navigation" id="block-mainnavigation" aria-labelledby="block-mainnavigation-menu" data-cd-toggable="Menu" data-cd-component="cd-nav" data-cd-icon="arrow-down" class="block block-menu navigation menu--main cd-nav cd-site-header__nav">
+<nav role="navigation"
+     id="block-mainnavigation"
+     aria-labelledby="block-mainnavigation-menu"
+     data-cd-toggable="Menu"
+     data-cd-component="cd-nav"
+     data-cd-icon="arrow-down"
+     class="block block-menu navigation menu--main cd-nav cd-site-header__nav"
+     >
     <h2 id="block-mainnavigation-menu">Main navigation</h2>
 	<?php
 		wp_nav_menu( array(
@@ -22,44 +29,3 @@
 	?>
 </nav>
 
-<!---->
-<!--<nav role="navigation" id="block-mainnavigation" aria-labelledby="block-mainnavigation-menu" data-cd-toggable="Menu"-->
-<!--     data-cd-component="cd-nav" data-cd-icon="arrow-down"-->
-<!--     class="block block-menu navigation menu--main cd-nav cd-site-header__nav" data-cd-processed="true">-->
-<!--    <h2 id="block-mainnavigation-menu">Main navigation</h2>-->
-<!--    <ul class="menu">-->
-<!--        <li class="menu-item menu-item--expanded">-->
-<!--            <ul data-cd-toggable="Layouts" data-cd-icon="arrow-down" data-cd-component="cd-main-menu"-->
-<!--                id="cd-main-menu-0-4" class="menu cd-main-menu__dropdown">-->
-<!---->
-<!--                <li class="menu-item">-->
-<!--                    <a href="/node/1" id="cd-main-menu-item-1-1"><span>First sidebar</span></a>-->
-<!--                </li>-->
-<!---->
-<!--                <li class="menu-item">-->
-<!--                    <a href="/node/2" id="cd-main-menu-item-1-2"><span>Second sidebar</span></a>-->
-<!--                </li>-->
-<!---->
-<!--                <li class="menu-item">-->
-<!--                    <a href="/node/3" id="cd-main-menu-item-1-3"><span>Both sidebars</span></a>-->
-<!--                </li>-->
-<!--            </ul>-->
-<!--        </li>-->
-<!---->
-<!--        <li class="menu-item menu-item--expanded">-->
-<!--            <ul data-cd-toggable="Views List" data-cd-icon="arrow-down" data-cd-component="cd-main-menu"-->
-<!--                id="cd-main-menu-0-5" class="menu cd-main-menu__dropdown">-->
-<!--                <li class="menu-item">-->
-<!--                    <a href="/teaser-list" id="cd-main-menu-item-1-1"><span>Teaser List</span></a>-->
-<!--                </li>-->
-<!--                <li class="menu-item">-->
-<!--                    <a href="/card-list" id="cd-main-menu-item-1-2"><span>Card List</span></a>-->
-<!--                </li>-->
-<!--                <li class="menu-item">-->
-<!--                    <a href="/facets" id="cd-main-menu-item-1-3"><span>List with facets</span></a>-->
-<!--                </li>-->
-<!--            </ul>-->
-<!--        </li>-->
-<!--    </ul>-->
-<!--</nav>-->
-<!---->
