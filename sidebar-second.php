fef8f92719393d85a22ba480b164a83973a280f3
left23 <lcdemc@gmail.com>
2021-07-07 14:57:32 +0200

    split sidebar into two partials

---
+++ sidebar-second.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template part for displaying sidebar content
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/
+ */
+?>
+
+<aside class="cd-layout-sidebar-second" role="complementary">
+	<?php dynamic_sidebar('Sidebar Second'); ?>
+</aside>
