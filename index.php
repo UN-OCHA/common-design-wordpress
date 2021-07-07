f793eb4401c4c99d31f5767194f4282be05f7122
left23 <lcdemc@gmail.com>
2021-07-07 15:20:14 +0200

    remove sidebar login from index template

--- index.php
+++ index.php
@@ -11,29 +11,19 @@
 <div class="cd-layout-highlighted cd-container">
 </div>
 
-<main role="main" id="main-content" class="cd-container cd-layout-with-sidebar-first">
-
-    <div class="cd-layout-content-wrapper">
-        <?php if ( !is_home() && !is_front_page() ) : ?>
-            <?php get_sidebar(); ?>
-        <?php endif; ?>
-
-        <div class="cd-layout-content">
-            <?php if ( have_posts() ) : ?>
-                <?php if ( is_home() && is_front_page() ) : ?>
-                    <?php get_template_part( 'resources/templates/content/content' ); ?>
-                <?php else : ?>
-                    <?php while ( have_posts() ) : ?>
-                        <?php the_post(); ?>
-
-                        <?php get_template_part( 'resources/templates/content/content', get_post_type() ); ?>
-                    <?php endwhile; ?>
-                <?php endif; ?>
+<main role="main" id="main-content" class="cd-container">
+    <div class="cd-layout-content">
+        <?php if ( have_posts() ) : ?>
+            <?php if ( is_home() && is_front_page() ) : ?>
+                <?php get_template_part( 'resources/templates/content/content' ); ?>
+            <?php else : ?>
+                <?php while ( have_posts() ) : ?>
+                    <?php the_post(); ?>
+                    <?php get_template_part( 'resources/templates/content/content', get_post_type() ); ?>
+                <?php endwhile; ?>
             <?php endif; ?>
-        </div>
-
+        <?php endif; ?>
     </div>
-
 </main>
 
 <?php get_footer(); ?>
