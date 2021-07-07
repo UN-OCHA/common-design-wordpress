10c7de1855110c615e45bb822f34e71f33dfa0a3
left23 <lcdemc@gmail.com>
2021-07-07 17:01:04 +0200

    display page title using tag or category name if it is a category or tag page

--- index.php
+++ index.php
@@ -12,6 +12,15 @@
 </div>
 
 <main role="main" id="main-content" class="cd-container">
+
+	<?php if ( have_posts() ) : ?>
+        <?php if ( is_category() or is_tag() ) : ?>
+            <h1 class="cd-page-title">
+                <?php echo single_cat_title(); ?>
+            </h1>
+        <?php endif; ?>
+	<?php endif; ?>
+
     <div class="cd-layout-content">
         <?php if ( have_posts() ) : ?>
             <?php if ( is_home() && is_front_page() ) : ?>
