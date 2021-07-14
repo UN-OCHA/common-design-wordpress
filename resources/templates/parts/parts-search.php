3f43952afb3978c7542027dc193453d5c8d3cffb
left23 <lcdemc@gmail.com>
2021-07-14 16:23:54 +0200

    add markup for dropdown search and change name value to q for strattic search

--- resources/templates/parts/parts-search.php
+++ resources/templates/parts/parts-search.php
@@ -1,23 +1,25 @@
 <?php
 /**
- * Search button in header.
- * Note the conditional $current_slug == 'search'
- * This means a page with the Permalink URL slug `search` needs to be created for the conditional to work.
+ * Search in header.
  */
 ?>
 
-<?php
-    global $wp;
-    $current_slug = add_query_arg( array(), $wp->request );
-?>
-
-<?php if ( $current_slug == 'search' ) : ?>
-	<a class="cd-search__btn cd-search__btn--active" href="/search">
-<?php else : ?>
-	<a class="cd-search__btn" href="/search">
-<?php endif ?>
-		<svg width="16" height="16" aria-hidden="true" focusable="false" class="cd-icon cd-icon--search cd-search__logo">
-			<use xlink:href="#cd-icon--search"></use>
-		</svg>
-		<span class="cd-search__btn-label">Search</span>
-	</a>
+<div class="cd-search">
+    <div class="search-block-form cd-search__form block block-search" aria-labelledby="cd-search-form" data-cd-toggable="Search" data-cd-component="cd-search" data-cd-logo="search" data-cd-focus-target="cd-search" id="block-searchform" role="search">
+        <h2 class="visually-hidden" id="cd-search-form">Search form</h2>
+        <form action="/search" method="get" id="search-block-form" accept-charset="UTF-8" class="cd-flow">
+            <div class="cd-form__item">
+                <label class="visually-hidden">Search</label>
+                <input title="Enter the terms you wish to search for." placeholder="What are you looking for?" class="cd-search__input form-search" id="cd-search" autocomplete="off" type="search" name="q" value="" size="15" maxlength="128">
+            </div>
+            <div class="form-actions js-form-wrapper form-wrapper" id="edit-actions">
+                <button class="cd-search__submit button js-form-submit form-submit" value="Search" type="submit">
+                    <svg class="cd-icon cd-icon--search" width="16" height="16">
+                        <use xlink:href="#cd-icon--search"></use>
+                    </svg>
+                    <span class="visually-hidden">Search</span>
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
