d1cb28704041133ce0743e6918a12aedb1e8532c
left23 <lcdemc@gmail.com>
2021-08-04 15:40:19 +0200

    add section element with flow when posts are displayed

--- index.php
+++ index.php
@@ -23,10 +23,13 @@
             <?php if ( is_home() && is_front_page() ) : ?>
                 <?php get_template_part( 'resources/templates/content/content' ); ?>
             <?php else : ?>
+
+            <section class="[ cd-flow ]">
                 <?php while ( have_posts() ) : ?>
                     <?php the_post(); ?>
                     <?php get_template_part( 'resources/templates/content/content', get_post_type() ); ?>
                 <?php endwhile; ?>
+            </section>
             <?php endif; ?>
         <?php endif; ?>
     </div>
