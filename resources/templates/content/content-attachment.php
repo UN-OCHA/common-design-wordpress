01d77a297aff0592f2046c70070233312afe7275
left23 <lcdemc@gmail.com>
2021-08-25 18:03:47 +0200

    add code comment

--- resources/templates/content/content-attachment.php
+++ resources/templates/content/content-attachment.php
@@ -1,1 +1,9 @@
+<?php
+/**
+ * Template part for displaying attachment post type
+ *
+ * @link https://developer.wordpress.org/themes/basics/linking-theme-files-directories/
+ */
+?>
+
 <?php get_template_part( 'resources/templates/common/common', 'article' ); ?>
